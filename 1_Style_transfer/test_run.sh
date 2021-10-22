6baa4f880015a6609973d8bcc541c635a9701d64
Sky24H <hytian2@gmail.com>
2021-10-22 09:58:46 +0000

    update

---
+++ 1_Style_transfer/test_run.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=1 python neural_style.py -style_image  -content_image  -output_image
---
+++ 3_ROTSS/test_run.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=1 python process_single.py --style_image  --content_image  --output_image
